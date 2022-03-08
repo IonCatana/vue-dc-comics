77a94099656a283ddab68a26b92496d1ff68e306
IonCatana <catana.ion17@yahoo.it>
2022-03-08 15:46:18 +0100

    Header finito

--- src/App.vue
+++ src/App.vue
@@ -23,11 +23,9 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  -moz-osx-font-smoothing: grayscale;  
+  margin-top: 50px;
 }
 </style>
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,19 +1,114 @@
 //HTML 
 <template>
   <div id="app">
-    <h1>MyHeader</h1>
-
+    <div class="header">
+      <div class="img_logo">
+        <img src="../assets/img/dc-logo.png" alt="" />
+      </div>
+      <div class="nav_bar">
+        <ul>
+          <li v-for="(link, i) in links" :key="i">
+            <a :href="link.href">{{ link.text }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 // JS script Vue
 <script>
 export default {
-  
-}
+  name: "MyHeader",
+  data() {
+    return {
+      links: [
+        {
+          text: "Characters",
+          href: "#",
+        },
+        {
+          text: "Comics",
+          href: "#",
+        },
+        {
+          text: "Movies",
+          href: "#",
+        },
+        {
+          text: "Tv",
+          href: "#",
+        },
+        {
+          text: "Games",
+          href: "#",
+        },
+        {
+          text: "Collectibles",
+          href: "#",
+        },
+        {
+          text: "Videos",
+          href: "#",
+        },
+        {
+          text: "Fans",
+          href: "#",
+        },
+        {
+          text: "News",
+          href: "#",
+        },
+        {
+          text: "Shop",
+          href: "#",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 // Style CSS
 <style lang="scss" scoped>
+#app {
+  width: 100vw;
+}
+img {
+  width: 100%;
+}
+.img_logo {
+  width: 80px;
+  height: 80px;
+}
+.header {
+  width: 70%;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+ul {
+  display: contents "";
+  list-style: none;
+  display: flex;
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 14px;
+  gap: 20px;
+}
 
+a {
+  text-decoration: none;
+  color: #1c1c1c;
+  &:hover {
+    color: blue;
+    border-bottom: 4px solid blue;
+    padding: 0px 0px 50px;
+  }
+  &:active {
+    color: red;
+    border-bottom: 4px solid red;
+  }
+}
 </style>
