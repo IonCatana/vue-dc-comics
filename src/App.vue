f0e55fb8ead9ef788a0314bca5293c0a5595be61
IonCatana <catana.ion17@yahoo.it>
2022-03-08 14:04:21 +0100

    Inizio esercizio

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <MyHeader />
+    
+  </div>
+</template>
+
+<script>
+
+import MyHeader from './components/MyHeader.vue'
+export default {
+  name: 'App',
+  components: {    
+    MyHeader
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="app">
+    <h1>MyHeader</h1>
+
+  </div>
+</template>
